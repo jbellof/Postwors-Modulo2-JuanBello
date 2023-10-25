a176b036d9f153f94b240593900108131469ac69
jbellof <90292783+jbellof@users.noreply.github.com>
2023-10-25 16:25:21 -0500

    PostworkS08
    Postwork de la clase #8 modulo java 2

---
+++ PosworkS08/src/test/java/com/example/posworks08/PosworkS08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.posworks08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosworkS08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
