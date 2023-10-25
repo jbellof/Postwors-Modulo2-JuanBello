0099aa33a8da1f5c13d3d32a4d4ff377458a2ef1
xaico <xaico@INGJUAN>
2023-10-25 15:46:41 -0500

    Initial commit

---
+++ src/main/java/com/example/posworks08/PosworkS08Application.java
@@ -0,0 +1,13 @@
+package com.example.posworks08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosworkS08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosworkS08Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/posworks08/service/ValidadorTelefono.java
@@ -0,0 +1,13 @@
+package com.example.posworks08.service;
+
+
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class ValidadorTelefono {
+    public String limpiaNumero(String telefono) {
+        return telefono.replaceAll("[^0-9]", "");
+    }
+}
