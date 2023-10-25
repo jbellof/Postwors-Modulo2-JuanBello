b53913f05c808e9671e67caeb8f68686a6b697ca
jbellof <90292783+jbellof@users.noreply.github.com>
2023-10-25 16:36:00 -0500

    PostworkS06
    Postwork de la clase #6 modulo java 2

---
+++ PosworkS06/src/main/java/com/example/posworks06/PosworkS06Application.java
@@ -0,0 +1,13 @@
+package com.example.posworks06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosworkS06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosworkS06Application.class, args);
+    }
+
+}
---
+++ PosworkS06/src/main/java/com/example/posworks06/persistence/AgendaMemoryDao.java
@@ -0,0 +1,22 @@
+package com.example.posworks06.persistence;
+import com.example.posworks06.model.Persona;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+@Repository
+public class AgendaMemoryDao {
+
+    private static final SortedSet<Persona> personas = new TreeSet<>();
+
+    public Persona guardaPersona(Persona persona) {
+        personas.add(persona);
+        return persona;
+    }
+
+    public Set<Persona> getPersonas() {
+        return personas;
+    }
+}
